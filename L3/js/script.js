8c811315cbaa8c6c4fd96dd06b649764df1bf848
Alex Andstén <alex.andsten@hotmail.com>
2021-02-04 18:51:37 +0100

    a

--- L3/js/script.js
+++ L3/js/script.js
@@ -7,7 +7,6 @@ var courseListElem;	// Referens till div-element där valda kurser ska läggas.
 function init() {
 	linkListElem = document.getElementById("linkList");
 	document.getElementById("linkBtn").addEventListener("click",listLinks);
-
 	linkInText = document.querySelectorAll("main section:nth-of-type(1) div a");
 	// Array med referenser till alla li-element i den andra section
 	let courseElems = document.querySelectorAll("main section:nth-of-type(2) div:first-of-type li");
@@ -16,45 +15,39 @@ function init() {
 		courseElems[i].style.cursor = "pointer";
 	}
 	courseListElem = document.getElementById("courseList");
-
 	document.getElementById("teacherBtn").addEventListener("click",addTeachers); // Används i extramerit
 } // End init
 window.addEventListener("load",init); // init aktiveras då sidan är inladdad
 // ---------------------------------------------------------------
 // Kopiera alla länkar ur huvudtexten och lägg upp dem i en lista.
 function listLinks() {
-	
 	for (let i = 0; i < linkInText.length; i++) {
-	let newElem = document.createElement("p");
-	let clonedElem = linkInText[i].cloneNode(true); // Klonat element
-	clonedElem.setAttribute("target", "_blank");
-	newElem.appendChild(clonedElem);
-	linkListElem.appendChild(newElem);
-	document.getElementById("linkBtn").removeEventListener("click",listLinks);
+		let newElem = document.createElement("p");
+		let clonedElem = linkInText[i].cloneNode(true); // Klonat element
+		clonedElem.setAttribute("target", "_blank");
+		newElem.appendChild(clonedElem);
+		linkListElem.appendChild(newElem);
+		document.getElementById("linkBtn").removeEventListener("click",listLinks);
 	}
-	
-
 } // End listLinks
 // ---------------------------------------------------------------
 // Den kurs användaren klickat på, läggs in överst i kurslistan.
 function addCourse(e) {
 	let newElem = document.createElement("p");
-		newElem.addEventListener("click",removeCourse);
-		newElem.style.cursor = "pointer";
-		let courseText = this.innerHTML;
-		let newTextNode = document.createTextNode(courseText); // Ny textnod
-		newElem.appendChild(newTextNode);
-		courseListElemP = document.getElementById("courseList").getElementsByTagName("p");
-	
-		for (let i = 0; i < courseListElemP.length; i++) {	
-			if (courseListElemP[i].innerHTML == this.innerHTML) {
-				return;
-			}	
-		}
-		let firstCourseInList = courseListElem.querySelector("p");
-		courseListElem.insertBefore(newElem,firstCourseInList);
+	newElem.addEventListener("click",removeCourse);
+	newElem.style.cursor = "pointer";
+	let courseText = this.innerHTML;
+	let newTextNode = document.createTextNode(courseText); // Ny textnod
+	newElem.appendChild(newTextNode);
+	courseListElemP = document.getElementById("courseList").getElementsByTagName("p");
+	for (let i = 0; i < courseListElemP.length; i++) {	
+		if (courseListElemP[i].innerHTML == this.innerHTML) {
+			return;
+		}	
+	}
+	let firstCourseInList = courseListElem.querySelector("p");
+	courseListElem.insertBefore(newElem,firstCourseInList);
 }
-
 // Den kurs användaren klickat på i kurslistan, tas bort.
 function removeCourse() {
 	this.parentNode.removeChild(this);
